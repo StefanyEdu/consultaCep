1ee35a462191a8aec5a44ccce3aaa336f24b58c6
Stefany Eduarda <ostefany20@gmail.com>
2021-11-08 16:18:44 -0300

    Atualizações tres

--- src/main/java/com/consultaCep/CepController.java
+++ src/main/java/com/consultaCep/CepController.java
@@ -1,6 +1,7 @@
 package com.consultaCep;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(exposedHeaders = "errors, content-type")
+@RequestMapping("/api/ceps")
 public class CepController {
 	
 	@Autowired
