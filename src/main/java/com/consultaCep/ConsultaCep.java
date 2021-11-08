f3871d7de3e48927a0e23c3b3a473f48ef6f985f
Stefany Eduarda <ostefany20@gmail.com>
2021-11-08 16:30:44 -0300

    atualização quatro

--- src/main/java/com/consultaCep/ConsultaCep.java
+++ src/main/java/com/consultaCep/ConsultaCep.java
@@ -10,38 +10,36 @@ public class ConsultaCep {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
-	private String cidade;
-	private String estado;
-	private String rua;
+	private String localidade;
+	private String bairro;
+	private String   logradouro;
 	private String cep;
 	
 	protected ConsultaCep() {}
-    public ConsultaCep(String cep ,String rua ,String cidade, String estado ) {
-    	this.cidade=cidade;
-    	this.estado=estado;
-    	this.rua= rua;
-    	this.cep=cep;
-    }
-    
-    @Override
-    public String toString() {
-    	return String.format("Consulta CEP[id=%d, cep='%s',rua='%s', cidade='%s',estado='%s']",id, cep,rua,cidade,estado);
-    }
-	
+
+	public ConsultaCep( String localidade, String bairro, String logradouro, String cep) {
+		
+		
+		this.localidade = localidade;
+		this.bairro = bairro;
+		this.logradouro = logradouro;
+		this.cep = cep;
+	}
+
 	public long getId() {
 		return id;
 	}
 
-	public String getCidade() {
-		return cidade;
+	public String getLocalidade() {
+		return localidade;
 	}
 
-	public String getEstado() {
-		return estado;
+	public String getBairro() {
+		return bairro;
 	}
 
-	public String getRua() {
-		return rua;
+	public String getLogradouro() {
+		return logradouro;
 	}
 
 	public String getCep() {
@@ -52,23 +50,26 @@ public class ConsultaCep {
 		this.id = id;
 	}
 
-	public void setCidade(String cidade) {
-		this.cidade = cidade;
+	public void setLocalidade(String localidade) {
+		this.localidade = localidade;
 	}
 
-	public void setEstado(String estado) {
-		this.estado = estado;
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
 	}
 
-	public void setRua(String rua) {
-		this.rua = rua;
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
 	}
 
 	public void setCep(String cep) {
 		this.cep = cep;
 	}
-	
-	
-	
 
+	@Override
+	public String toString() {
+		return "ConsultaCep [id=" + id + ", localidade=" + localidade + ", bairro=" + bairro + ", logradouro="
+				+ logradouro + ", cep=" + cep + "]";
+	}
+	
 }
